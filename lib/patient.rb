00a34e752a89f05dc07f8410eaf9291dae258a70
Josh Cesana <joshuacesana@gmail.com>
2018-05-26 23:12:39 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,6 +12,19 @@ class Patient
   end 
   
   def new_appointment(doctor, date)
-    Appointment.new()
+    Appointment.new(date, self, doctor)
   end 
+  
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self 
+    end 
+  end 
+  
+  def doctors
+    appointments.map do |appointment|
+      appointment.doctor 
+    end 
+  end 
+  
 end 
