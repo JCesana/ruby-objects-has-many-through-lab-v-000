0778fc4127dd9a19426463a929048db2444f31fc
Josh Cesana <joshuacesana@gmail.com>
2018-05-26 21:41:12 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,28 +1,4 @@
-require 'pry'
 class Artist 
-  @@all = []
-  attr_accessor :name, :songs, :all
   
-  def initialize(name)
-    @name = name 
-    @songs = []
-    @@all << self 
-  end 
-  
-  
-  def new_song(name, genre)
-    song = Song.new(name, self, genre)
-    @songs << song
-  end
-  
-  
-  def songs 
-    Song.all.select { |song| song.artist == self }
-  end 
-  
-  
-  def self.all 
-    @@all 
-  end 
   
 end 
