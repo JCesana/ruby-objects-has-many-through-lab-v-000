43e1c0aae98288eda6dbf74793a5d053c1354971
Josh Cesana <joshuacesana@gmail.com>
2018-05-26 23:11:03 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,7 +1,17 @@
 class Patient
   @@all = []
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
   
   def self.all
     @@all 
+  end 
+  
+  def new_appointment(doctor, date)
+    Appointment.new()
   end 
 end 
