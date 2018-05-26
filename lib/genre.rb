8bdfa0371557045d9dddab15440e4bbf171ee9fe
Josh Cesana <joshuacesana@gmail.com>
2018-05-26 21:01:29 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,9 +1,15 @@
 class Genre 
+  @@all = []
   attr_accessor :name, :songs
   
   def initialize(name)
     @name = name
     @songs = [] 
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all 
   end 
   
 end 
